04eac8ad0a49fbcb193ce9c726c867cd7fa0a41b
Fedorenko <Fedorenko@IT-64>
2016-04-25 12:35:28 +0300

    test1

--- FirstVB2015/FirstVB2015/FormHelloWorld.vb
+++ FirstVB2015/FirstVB2015/FormHelloWorld.vb
@@ -1,5 +1,6 @@
 ﻿Public Class FormHelloworld
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox "Hello world!!!"
+        MsgBox("Hello wonderfull world!!!", vbOK)
+
     End Sub
 End Class
